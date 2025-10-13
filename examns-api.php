894f7a5831a754da46b9fc5615fc0e4a67d72101
rvst312 <aaronchmanda25@gmail.com>
2025-10-13 17:41:44 +0300

    feat: add exam rewrite rules and update plugin version to 2.0.0
    - Add new rewrite rules for exam-related pages
    - Rename activation/deactivation functions for clarity
    - Move assets enqueue function to top of file

--- examns-api.php
+++ examns-api.php
@@ -4,7 +4,7 @@
  * Plugin Name:       Exercises & Exams API
  * Plugin URI:        https://versatile-handbook-314758.framer.app/page
  * Description:       This plugin provides a simple way to integrate exercises and exams into your WordPress site.
- * Version:           1.0.0
+ * Version:           2.0.0
  * Author:            Aarón RV
  * Author URI:        https://www.linkedin.com/in/aar%C3%B3n-franco-fern%C3%A1ndez/
  * License:           GPL-2.0+
@@ -39,6 +39,14 @@ foreach ($includes as $file) {
     }
 }
 
+// Enqueue assets
+function enqueue_assets()
+{
+    wp_enqueue_style('examens-style', plugin_dir_url(__FILE__) . 'assets/css/style.css');
+    wp_enqueue_script('examens-script', plugin_dir_url(__FILE__) . 'assets/js/scripts.js', ['jquery'], null, true);
+}
+add_action('wp_enqueue_scripts', 'enqueue_assets');
+
 // Rewrite URLs for friendly links
 function add_exercise_rewrite_rules()
 {
@@ -53,8 +61,25 @@ function add_exercise_rewrite_rules()
 
     // Regla para la página "exercicis" y un "subject"
     add_rewrite_rule('^exercicis-selectivitat/assignatura/([^/-][^/]*?)/?$', 'index.php?pagename=assignatura&subject=$matches[1]', 'top');
+
 }
 add_action('init', 'add_exercise_rewrite_rules');
+
+function add_exam_rewrite_rules()
+{
+    // Regla para la página "examenes" con un ID
+    add_rewrite_rule('^examenes/([a-zA-Z0-9-_]+)/?$', 'index.php?pagename=examenes&id=$matches[1]', 'top');
+
+    // Regla para la página "solucio" con un ID
+    add_rewrite_rule('^examenes/solucio/([a-zA-Z0-9-_]+)/?$', 'index.php?pagename=examenes&id=$matches[1]', 'top');
+
+    // Regla para la página "examens-de-selectivitat" y un "subject"
+    add_rewrite_rule('^examens-de-selectivitat/asignatura/([^/-][^/]*?)/?$', 'index.php?pagename=asignatura&subject=$matches[1]', 'top');
+	
+    // Regla para la página "examens-de-selectivitat" y un año
+    add_rewrite_rule('^examens-de-selectivitat/year/([a-zA-Z0-9-_]+)/?$', 'index.php?pagename=year&year=$matches[1]', 'top');
+}
+add_action('init', 'add_exam_rewrite_rules');
 
 // Register query variables
 function register_exercise_query_vars($vars)
@@ -72,6 +97,9 @@ function clean_subject_parameter($value)
     if (get_query_var('pagename') === 'exercicis' && get_query_var('subject')) {
         return str_replace('-', ' ', urldecode($value));
     }
+    else if (get_query_var('pagename') === 'examens' && get_query_var('subject')) {
+        return str_replace('-', ' ', urldecode($value));
+    }
     return $value;
 }
 add_filter('request', function ($vars) {
@@ -81,24 +109,17 @@ add_filter('request', function ($vars) {
     return $vars;
 });
 
-// Enqueue assets
-function enqueue_assets()
-{
-    wp_enqueue_style('examens-style', plugin_dir_url(__FILE__) . 'assets/css/style.css');
-    wp_enqueue_script('examens-script', plugin_dir_url(__FILE__) . 'assets/js/scripts.js', ['jquery'], null, true);
-}
-add_action('wp_enqueue_scripts', 'enqueue_assets');
-
 // Flush rewrite rules on activation/deactivation
-function examens_activate()
+function flush_rewrite_rules_on_activation()
 {
     add_exercise_rewrite_rules();
+    add_exam_rewrite_rules();
     flush_rewrite_rules();
 }
-register_activation_hook(__FILE__, 'examens_activate');
+register_activation_hook(__FILE__, 'flush_rewrite_rules_on_activation');
 
-function examens_deactivate()
+function flush_rewrite_rules_on_deactivation()
 {
     flush_rewrite_rules();
 }
-register_deactivation_hook(__FILE__, 'examens_deactivate');
+register_deactivation_hook(__FILE__, 'flush_rewrite_rules_on_deactivation');
