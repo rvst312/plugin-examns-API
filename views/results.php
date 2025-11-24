c33605ed9810e8573befad5ebc14c7e4055b74d9
rvst312 <aaronchmanda25@gmail.com>
2025-11-24 10:10:22 +0200

    fix: styles to '.grid-item'

--- views/results.php
+++ views/results.php
@@ -36,7 +36,7 @@ function mostrar_resultados($response)
                 $theme_green = '#d9ed927c';
                 $theme_red = '#ffb38786';
                 // Default: Orange 
-                $bg_color = $theme_red; 
+                $bg_color = $theme_red;
 
                 if ($item['tipus_prova'] === 'PAP') {
                     $bg_color = $theme_blue;
@@ -48,17 +48,17 @@ function mostrar_resultados($response)
 
                 ?>
                 <div class="grid-item" style="background-color: <?= htmlspecialchars($bg_color); ?>;">
-                    <img src="<?= isset($item['url_miniatura']) ? htmlspecialchars($item['url_miniatura']) : 'placeholder.jpg'; ?>"
-                        alt="Miniatura"
-                        loading="lazy"
-                        onload="this.style.opacity='1'">
                     <p style="text-align: center;">
                         <span class="category"><?= isset($item['tipus_prova']) ? htmlspecialchars($item['tipus_prova']) : 'Desconegut'; ?></span>
-                        <!--<span class="theme"><?= isset($item['comunitat']) ? htmlspecialchars($item['comunitat']) : 'Desconegut'; ?></span><br />-->
+                        <span class="theme"><?= isset($item['comunitat']) ? htmlspecialchars($item['comunitat']) : 'Desconegut'; ?></span><br />
                         <span class="subject"><?= isset($item['assignatura']) ? htmlspecialchars($item['assignatura']) : 'Desconegut'; ?></span>
                         <span class="convocatoria"><?= isset($item['convocatoria']) ? htmlspecialchars($item['convocatoria']) : 'Desconegut'; ?></span>
                         <span class="year"><?= isset($item['any']) ? htmlspecialchars($item['any']) : 'Desconegut'; ?></span>
                     </p>
+                    <img src="<?= isset($item['url_miniatura']) ? htmlspecialchars($item['url_miniatura']) : 'placeholder.jpg'; ?>"
+                        alt="Miniatura"
+                        loading="lazy"
+                        onload="this.style.opacity='1'">
                     <div class="buttons-item">
                         <?php if (isset($item['tipus_resultat']) && $item['tipus_resultat'] === 'examen'): ?>
                             <a class="button-secondary-exams"
